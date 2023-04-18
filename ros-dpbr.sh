14ed57f2f54909bae79f90c3094dfeccfc88c469
qiutao1204 <9339861+qiutao1204@users.noreply.github.com>
2023-04-18 18:33:47 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -9,7 +9,7 @@ wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/jacyl
 echo "/ip firewall address-list"
 
 for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+  echo "add list=List_ALL_China address=$net comment=chnroute"
 done
 
 } > ../CN.rsc
