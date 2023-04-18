edd257290c9106493e10e7b08da7ecd47bad9062
qiutao1204 <9339861+qiutao1204@users.noreply.github.com>
2023-04-18 18:32:09 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,7 +3,7 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/jacyl4/chnroute/main/IPchnroute
 
 {
 echo "/ip firewall address-list"
