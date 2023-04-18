f95cbf0eba8ddee794e0fb1070a6d0263c8bd558
qiutao1204 <9339861+qiutao1204@users.noreply.github.com>
2023-04-18 18:26:25 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,16 +3,16 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/jacyl4/chnroute/main/IPchnroute
 
 {
 echo "/ip firewall address-list"
 
 for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+  echo "add list=List_ALL_China address=$net comment=chnroute"
 done
 
-} > ../CN.rsc
+} > ../.rsc
 
 cd ..
 rm -rf ./pbr
